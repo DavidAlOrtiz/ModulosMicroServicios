c4d6896543dffcbbe59a6fc35653a69e6fa22bc7
David Ortiz <david.al.ortiz.mtz@gmail.com>
2023-10-04 06:49:44 -0600

    clase intermedia

--- ms-cursos/src/main/java/com/dva/springboot/msvc/cursos/mscursos/entities/models/Curso.java
+++ ms-cursos/src/main/java/com/dva/springboot/msvc/cursos/mscursos/entities/models/Curso.java
@@ -1,11 +1,14 @@
 package com.dva.springboot.msvc.cursos.mscursos.entities.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -21,5 +24,20 @@ public class Curso implements Serializable {
 
     @NotBlank
     private String name;
+
+    @OneToMany
+    private List<CursoUsuario> cursoUsuarios;
+
+    public Curso() {
+        cursoUsuarios = new ArrayList<>();
+    }
+
+    public void agregarUsaurio(CursoUsuario cursoUsuario) {
+        this.cursoUsuarios.add(cursoUsuario);
+    }
+
+    public void eliminarUsario(CursoUsuario cursoUsuario) {
+        this.cursoUsuarios.remove(cursoUsuario);
+    }
 
 }
